15ecda97ee539d07e3223b61e1106196b909ead9
kleber.barilli <kleber.barilli73@gmail.com>
2023-02-22 20:47:45 -0300

    add telephone

--- src/main/java/br/com/Student.java
+++ src/main/java/br/com/Student.java
@@ -1,7 +1,16 @@
 package br.com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private Email email;
     private Cpf cpf;
+
+    private List<Telephone> telephones = new ArrayList<>();
+
+    public void addTelephone(String ddd, String number) {
+        this.telephones.add(new Telephone(ddd, number));
+    }
 }
---
+++ src/main/java/br/com/Telephone.java
@@ -0,0 +1,11 @@
+package br.com;
+
+public class Telephone {
+    private String ddd;
+    private String number;
+
+    public Telephone(String ddd, String number) {
+        this.ddd = ddd;
+        this.number = number;
+    }
+}

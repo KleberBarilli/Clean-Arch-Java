608765f483fe5cd96cb8d5afbb9b2f4f0c366c56
kleber.barilli <kleber.barilli73@gmail.com>
2023-02-23 21:18:45 -0300

    teste windows

--- src/test/java/br/com/school/domain/TelephoneTest.java
+++ src/test/java/br/com/school/domain/TelephoneTest.java
@@ -3,6 +3,7 @@ package br.com.school.domain;
 import br.com.school.domain.student.Telephone;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TelephoneTest {
